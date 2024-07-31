66be568e809c86fbe660ef005d8fe4de09696eb6
Repo6798 <Manishraj0494@gmail.com>
2024-07-31 13:38:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# devggn
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", "28238792"))
+API_HASH = getenv("API_HASH", "89b0d1f463a860525e1c117ccdef5aa3")
+BOT_TOKEN = getenv("BOT_TOKEN", "6360468548:AAEVcrVqSKsdRfivNrSMos4xkkA3syB_Qiw")
+OWNER_ID = int(getenv("OWNER_ID", "1236569704"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://shubhp97:RI9LlWYGuMIJFJ1w@mrpanchall.p0lnx7k.mongodb.net/?retryWrites=true&w=majority&appName=Mrpanchall")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002151804796"))
+FORCESUB = getenv("FORCESUB", "Coursessffh")
